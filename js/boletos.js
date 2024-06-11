5ae7a03396fb60a5f74ceb22bd0d575527a88b90
Zeleyda <daniel00moore@outlook.es>
2024-06-11 14:42:37 -0600

    ya se cierra el bottomsheet de boletos apartados

--- js/boletos.js
+++ js/boletos.js
@@ -8,7 +8,20 @@ document.addEventListener('DOMContentLoaded', () => {
     closeBtn.addEventListener('click', () => {
         selectedNumbersBottomsheet.classList.remove('show');
     });
+
+    const toggleSizeBtn = document.createElement('button');
+    toggleSizeBtn.classList.add('toggle-size-btn');
+    toggleSizeBtn.innerHTML = '&#8645;'; // Icono para cambiar el tamaño
+    toggleSizeBtn.addEventListener('click', () => {
+        if (selectedNumbersBottomsheet.style.maxHeight === '50vh') {
+            selectedNumbersBottomsheet.style.maxHeight = '90vh';
+        } else {
+            selectedNumbersBottomsheet.style.maxHeight = '50vh';
+        }
+    });
+
     selectedNumbersBottomsheet.insertBefore(closeBtn, selectedNumbersBottomsheet.firstChild);
+    selectedNumbersBottomsheet.insertBefore(toggleSizeBtn, selectedNumbersBottomsheet.firstChild);
 
     for (let i = 1; i <= 5000; i++) {
         const button = document.createElement('button');
@@ -27,6 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
         selectedButtons.forEach(button => {
             const clone = button.cloneNode(true);
             clone.classList.remove('ticket-container');
+            clone.classList.add('available');
             selectedNumbersDiv.appendChild(clone);
         });
         if (selectedButtons.length > 0) {
