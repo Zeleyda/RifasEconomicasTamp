e728039e2bc3bed55516730ff1662d9ac568cd30
ELCesarMaat <34516377+ELCesarMaat@users.noreply.github.com>
2024-06-11 18:39:01 -0600

    Update bdScript.sql

--- backend/bdScript.sql
+++ backend/bdScript.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE `rifasdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 CREATE TABLE `rifas` (
   `RifaId` int NOT NULL AUTO_INCREMENT,
   `RifaName` varchar(255) NOT NULL,
