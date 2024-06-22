bdd03efec3bfc9634e4e3eb8815742e43a6ec8cd
Zeleyda <daniel00moore@outlook.es>
2024-06-22 01:56:22 -0600

    Se modifico algunos diseños y tambien se agrego pagina de aviso de privacidad

---
+++ sections/aviso_privacidad.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RIFAS ECONOMICAS TAMPICO</title>
+    <link rel="stylesheet" href="/RifasEconomicasTamp/styles/info.css">
+    <link rel="stylesheet" href="/RifasEconomicasTamp/styles/background.css">
+    <link rel="stylesheet" href="/RifasEconomicasTamp/styles/navbar.css">
+    <link rel="stylesheet" href="/RifasEconomicasTamp/styles/footer.css">
+    <link rel="stylesheet" href="/RifasEconomicasTamp/styles/cuentas.css">
+  
+</head>
+<body>
+<div class="background-image"></div>
+    <div class="overlay"></div>
+    <div class="content">
+    <?php include 'navbar.html'; ?>
+    
+    <h2 class="container-h1">MÉTODOS DE PAGO</h2>
+            <div class="container" >
+                <p>
+Su privacidad y confianza son muy importantes para nosotros, por ello queremos asegurarnos que conozca como salvaguardamos la integridad, privacidad y protección de sus datos personales, por lo que se informa a usted los términos en que serán tratados los datos personales que recaben, por lo cual, le pedimos que lea atentamente la siguiente información:
+
+1. Definiciones.
+De conformidad con el artículo 3 de la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, se entenderá como:
+
+    Aviso de Privacidad: Documento físico, electrónico o en cualquier otro formato generado por el responsable que es puesto a disposición del titular, previo al tratamiento de sus datos personales.
+    Consentimiento: Manifestación de la voluntad del titular de los datos mediante la cual se efectúa el tratamiento de los mismos.
+    Datos personales: Cualquier información concerniente a una persona física identificada o identificable.
+    Datos personales sensibles: Aquellos datos personales que afecten a la esfera más íntima de su titular, o cuya utilización indebida pueda dar origen a discriminación o conlleve un riesgo grave para éste. En particular, se consideran sensibles aquellos que puedan revelar aspectos como origen racial o étnico, estado de salud presente y futuro, información genética, creencias religiosas, filosóficas y morales, afiliación sindical, opiniones políticas, preferencia sexual.
+    Responsable: Persona física o moral de carácter privado que decide sobre el tratamiento de datos personales.
+    Tercero: La persona física o moral, nacional o extranjera, distinta del titular o del responsable de los datos.
+    Titular: La persona física a quien corresponden los datos personales.
+    Transferencia: Toda comunicación de datos realizada a persona distinta del responsable o encargado del tratamiento.
+
+
+2. Identidad y domicilio del responsable.
+La empresa con domicilio en la es el responsable del tratamiento de los datos personales que nos proporcione, los cuales serán protegidos conforme a lo dispuesto a la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, y demás normatividad que resulte aplicable.
+
+3. Datos personales recabados por el responsable.
+Para nosotros resulta necesaria la recopilación de ciertos datos personales para llevar a cabo las actividades intrínsecas a su giro, la empresa tiene la obligación legal y social de cumplir con las medidas, legales y de seguridad suficientes para proteger aquellos datos personales que haya recabado para las finalidades que en la presente política de privacidad serán descritas.
+Para llevar a cabo el cumplimiento de las finalidades señaladas en la cláusula 4° cuarta del presente aviso de privacidad, el responsable le informa que podremos recabar los siguientes datos personales:
+    I.	Datos de Identificación;
+    II.	Datos de Contacto;
+    III.	Datos bacarios;
+Con relación al texto que antecede, le informamos que los Datos Personales Sensibles que se recaben del Titular serán obtenidos únicamente en forma directa; es decir en forma personal o a través de los Medios Electrónicos que se pongan a disposición, en el entendido de que los Datos Personales Sensibles recabados, se tratarán con la finalidad exclusiva de realizar y dar seguimiento al proceso de compra y/o arrendamiento que será realizado.
+El responsable podrá realizar las investigaciones, realizar entrevistas y en general, realizar las acciones que considere necesarias con motivo de poder comprobar por sí o a través de terceros contratados únicamente para ello, la veracidad de los datos personales que les fueron proporcionados por el Titular.
+El Titular manifiesta que los datos personales proporcionados al responsable tienen veracidad y se encuentran actualizados, y se obliga a comunicar al responsable cualquier modificación que se lleve a cabo con la mayor brevedad posible.
+
+4. Finalidad del tratamiento de datos.
+Los datos personales que recabemos de usted, los utilizaremos para las siguientes finalidades:
+    I.	Corroborar la identidad del Titular y la veracidad de la información que proporcione como propia;
+    II.	La creación e implementación de procesos analíticos y estadísticos necesarios para la prestación de nuestros servicios;
+    III.	Evaluar la calidad del servicio que brindamos y en general para dar cumplimiento a las obligaciones que hemos contraído con usted;
+    IV.	Formalizar la relación entre el Titular y el responsable y prestar los servicios financieros necesarios para llevar a cabo las transacciones, así como formalizar la relación que existe entre las partes y en su caso, requerir el cumplimiento.
+
+5. Límites de los titulares para el uso o divulgación de datos.
+Se le informa que usted Titular puede solicitar la actualización, los límites o divulgación o cualquier cuestión referente a la información brindada sobre los datos personales brindada al Responsable, al correo electrónico .
+Usted tiene derecho a conocer qué datos personales se tienen de usted, para qué se utilizan y las condiciones del uso que les damos (Acceso). Asimismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea inexacta o incompleta (Rectificación); que la eliminemos de nuestros registros o bases de datos cuando considere que la misma no está siendo utilizada conforme a los principios, deberes y obligaciones previstas en la ley (Cancelación); así como oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como derechos ARCO y podrá ejercerlos en un horario de por medio de los datos de contacto antes referidos, a lo cual la empresa tendrá 15 (quince) días naturales para dar respuesta a su petición.
+
+6. Transferencia de datos personales.
+Se le informa que sus datos personales no serán compartidos con terceras personas como empresas, organizaciones o autoridades, únicamente el Responsable tendrá acceso a ellos en los términos del presente aviso de privacidad.
+
+7. Cambios al aviso de privacidad.
+Si se llegase a hacer alguna modificación al Aviso de Privacidad, se le hará de su conocimiento mediante la publicación de este en nuestra página oficial, la cual se encuentra a su disposición las 24 horas del día los 365 días del año.
+
+8. Aceptación del presente aviso.
+La lectura del presente Aviso de Privacidad constituye en sí misma la aceptación del mismo, por lo que si no se encuentra conforme con las políticas de privacidad le pedimos que nos lo haga saber a la brevedad para poder brindarle una mejor atención en los datos de contacto precisados.</p>
+            </div>
+
+    <?php include 'footer.html'; ?>
+</body>
+</html>
+
